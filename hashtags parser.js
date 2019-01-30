2bd5cdaa4e46c23b9a97d9e6399afbb8cff79f1d
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-30 16:10:42 +0800

    Initial commit 	New Repo called tools to store some of the tools that I build for myself 	to use to automate some stuff and more.
    	Currently working on a tool to help me parse hashtags into a simple python
    	code to use with the InstaPy program.

---
+++ hashtags parser.js
@@ -0,0 +1,42 @@
+'use strict'; // Use strict version of JavaScript
+
+const fs = require('fs');
+
+// const log = (str) => console.log(str);
+
+const file_name = 'hashtags.txt';
+
+// Read the file into memory
+let file_content = fs.readFileSync(`${file_name}`, 'utf8');
+// Split all the tags with the '#' character into an array
+file_content = file_content.split('#');
+// Get the number of elements in the array
+let len = file_content.length;
+
+// Loop through all the elements in the array
+while (--len) {
+	// Remove any empty elements from the array
+	if (!file_content[len]) {
+		file_content.splice(len, 1);
+		continue; // Skip this loop cycle
+	}
+	// Remove all white spaces in the words
+	file_content[len] = file_content[len].trim();
+	// Create the word wrap for the tags.
+	file_content[len] = `"${file_content[len]}", `;
+}
+
+// Create and clean up the string by joining the array, triming off whitespaces and removing the last comma
+file_content = file_content.join('').trim().slice(0, file_content.lastIndexOf(','));
+// Finnish up the python code generation.
+file_content = `session.like_by_tags([${file_content}], amount=${30})`
+// Write the contents to another file for the user to use.
+fs.writeFileSync(`transformed_${file_name}`, file_content);
+
+
+/*	Example:
+	input:
+		#hashtag #hashtag1 #hashtag2 #hashtag3 
+	output:
+		session.like_by_tags(["hashtag", "hashtag1", "hashtag2", "hashtag3"], amount=30)
+*/
